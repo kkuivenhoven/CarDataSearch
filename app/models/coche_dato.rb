dddcc8b243a224394e422ed321825bc2590f90d4
Kendra Kuivenhoven <kkuivenhoven@mail.csuchico.edu>
2022-02-02 21:20:57 -0800

    Have added search ability for horsepwer; for a range of horsepower; have added search by model (year); have also refactored code in controller

--- app/models/coche_dato.rb
+++ app/models/coche_dato.rb
@@ -8,7 +8,42 @@ class CocheDato < ApplicationRecord
 			indexes :model, type: :text, analyzer: :english
 			indexes :origin, type: :text, analyzer: :english
 			indexes :mpg, type: :text, analyzer: :english
+			indexes :horsepower, type: :text, analyzer: :english
 		end
+	end
+
+	def self.searchByCountry(country_origin)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+ 				bool: {
+ 					must: [
+ 					{
+ 						match: {
+ 							origin: country_origin
+ 						}
+ 					},
+ 					]
+ 				}
+ 			}
+ 		})
+	end
+
+	def self.searchByHorsepower(horsepower)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+ 				bool: {
+ 					must: [
+ 					{
+ 						match: {
+ 							horsepower: horsepower
+ 						}
+ 					},
+ 					]
+ 				}
+ 			}
+ 		})
 	end
 
 	def self.searchByCarName(query)
@@ -149,6 +184,140 @@ class CocheDato < ApplicationRecord
  		})
 	end
 
+	def self.searchByModel(model)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+ 				bool: {
+ 					must: [
+ 					{
+ 						match: {
+ 							model: model
+ 						}
+ 					},
+ 					]
+ 				}
+ 			}
+ 		})
+	end
+
+	def self.searchByQueryModel(query, model)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+ 				bool: {
+ 					must: [
+ 					{
+ 						match: {
+ 							car: query
+						},
+					},
+					{
+						match: {
+							model: model	
+ 						}
+ 					},
+ 					]
+ 				}
+ 			}
+ 		})
+	end
+
+	def self.searchByQueryModelCountry(query, model, country_origin)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+ 				bool: {
+ 					must: [
+ 					{
+ 						match: {
+ 							car: query
+						},
+					},
+					{
+						match: {
+							model: model	
+ 						}
+ 					},
+					{
+						match: {
+							origin: country_origin	
+ 						}
+					}
+ 					]
+ 				}
+ 			}
+ 		})
+	end
+
+	def self.searchByQueryModelCountryMpg(query, model, country_origin, mpg)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+ 				bool: {
+ 					must: [
+ 					{
+ 						match: {
+ 							car: query
+						},
+					},
+					{
+						match: {
+							model: model	
+ 						}
+ 					},
+					{
+						match: {
+							origin: country_origin	
+ 						}
+					},
+					{
+						match: {
+							mpg: mpg	
+ 						}
+					}
+ 					]
+ 				}
+ 			}
+ 		})
+	end
+
+	def self.searchByQueryModelCountryMpgRange(query, model, country_origin, mpg, mpg_two)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+ 				bool: {
+ 					must: [
+ 					{
+ 						match: {
+ 							car: query
+						},
+					},
+					{
+						match: {
+							model: model	
+ 						}
+ 					},
+					{
+						match: {
+							origin: country_origin	
+ 						}
+					},
+					{
+						range: {
+							mpg: {
+								gte: mpg,
+								lte: mpg_two,
+								boost: 2.0
+							}
+						}
+					}
+ 					]
+ 				}
+ 			}
+ 		})
+	end
+
 	def self.searchByMpgCountry(mpg, country_origin)
  		self.search({
  			size: 25,
@@ -207,6 +376,144 @@ class CocheDato < ApplicationRecord
 						lte: mpg_two,
 						boost: 2.0
 					}
+				}
+ 			}
+ 		})
+	end
+
+	def self.searchByHorsepowerRange(horsepower, horsepower_two)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+				range: {
+					horsepower: {
+						gte: horsepower,
+						lte: horsepower_two,
+						boost: 2.0
+					}
+				}
+ 			}
+ 		})
+	end
+
+	def self.searchByHorsepowerRangeQuery(horsepower, horsepower_two, query)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+				bool: {
+					must: [
+					{
+						multi_match: {
+							query: query,
+							fields: [:car, :model, :origin]
+						}
+					},
+						range: {
+							horsepower: {
+								gte: horsepower,
+								lte: horsepower_two,
+								boost: 2.0
+							}
+					}]
+				}
+ 			}
+ 		})
+	end
+
+	def self.searchByHorsepowerRangeQueryCountry(horsepower, horsepower_two, query, country_origin)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+				bool: {
+					must: [
+					{
+						multi_match: {
+							query: query,
+							fields: [:car, :model]
+						}
+					},
+					{
+						match: {
+							origin: country_origin
+						}
+					},
+					range: {
+						horsepower: {
+							gte: horsepower,
+							lte: horsepower_two,
+							boost: 2.0
+						}
+					}]
+				}
+ 			}
+ 		})
+	end
+
+	def self.searchByHorsepowerRangeQueryCountryMpg(horsepower, horsepower_two, query, country_origin, mpg)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+				bool: {
+					must: [
+					{
+						multi_match: {
+							query: query,
+							fields: [:car, :model]
+						}
+					},
+					{
+						match: {
+							origin: country_origin
+						}
+					},
+					{
+						match: {
+							mpg: mpg
+						}
+					},
+					range: {
+						horsepower: {
+							gte: horsepower,
+							lte: horsepower_two,
+							boost: 2.0
+						}
+					}]
+				}
+ 			}
+ 		})
+	end
+
+	def self.searchByHorsepowerRangeQueryCountryMpgRange(horsepower, horsepower_two, query, country_origin, mpg, mpg_two)
+ 		self.search({
+ 			size: 25,
+ 			query: {
+				bool: {
+					must: [
+					{
+						multi_match: {
+							query: query,
+							fields: [:car, :model]
+						}
+					},
+					{
+						match: {
+							origin: country_origin
+						}
+					},
+					range: {
+						horsepower: {
+							gte: horsepower,
+							lte: horsepower_two,
+							boost: 2.0
+						}
+					},
+					range: {
+						mpg: {
+							gte: mpg,
+							lte: mpg_two,
+							boost: 2.0
+						}
+					}]
 				}
  			}
  		})
